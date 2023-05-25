74e0342cd29bfc4a8ad0d13be9b932519613e04b
horozal <36187605+horozal@users.noreply.github.com>
2023-05-25 20:35:19 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print('Hello world!')
+from space_manager import SpaceManager
+from in_memory_grid import InMemoryGrid
+
+# Usage example
+if __name__ == '__main__':
+    # Create and start the Space Manager
+    space_manager = SpaceManager()
+    space_manager.start()
+
+    # Create and initialize the In-Memory Grid
+    in_memory_grid = InMemoryGrid()
+    in_memory_grid.initialize()
+
+    # Perform necessary operations with the space-based architecture
+    # ...
+
+    # Stop the Space Manager and In-Memory Grid
+    space_manager.stop()
+    in_memory_grid.stop()
