59ee16caaa8930d70731f03f609bf063a7599554
horozal <36187605+horozal@users.noreply.github.com>
2023-05-25 20:33:29 +0200

    Update space_manager.py

--- src/space_manager/space_manager.py
+++ src/space_manager/space_manager.py
@@ -2,6 +2,9 @@ from multiprocessing import Process, Queue
 
 # Define the space-based architecture manager
 class SpaceManager:
-    def __init__(self):
-        self.queue = Queue()
+    def start(self):
+        print("Space Manager started.")
+
+    def stop(self):
+        print("Space Manager stopped.")
       
