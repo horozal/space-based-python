a710981370504bdc4078aa91c8328dc236614b93
horozal <36187605+horozal@users.noreply.github.com>
2023-05-25 20:34:19 +0200

    Update in_memory_grid.py

--- src/in_memory_grid/in_memory_grid.py
+++ src/in_memory_grid/in_memory_grid.py
@@ -2,3 +2,9 @@
 class InMemoryGrid:
     def __init__(self):
         self.spaces = {}
+    
+    def initialize(self):
+        print("In-Memory Grid initialized.")
+
+    def stop(self):
+        print("In-Memory Grid stopped.")
